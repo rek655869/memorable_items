96282ab0c5b53163871278d4338ddcd56a3acfdc
rek655689 <e.gladneva1357@gmail.com>
2024-12-13 21:48:05 +0300

    столбцы ID и Имя поменяны местами

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -12,14 +12,14 @@ class Player {
         this._id = _id;
         this._sex = _sex;
 
-        let range: Range = PLAYERS_LIST.getRange(3, 3, PLAYERS_LIST.getLastRow() - 2, 1);
+        let range: Range = PLAYERS_LIST.getRange(3, 2, PLAYERS_LIST.getLastRow() - 2, 1);
         let finder: TextFinder = range.createTextFinder(String(this._id)).matchEntireCell(true);
         let result: Range | null = finder.findNext();
         if (!result) {
             throw new Error("Игрока с таким ID не существует");
         }
         this._rowIndex = result.getRow();
-        let rowData: any[] = PLAYERS_LIST.getRange(this._rowIndex, 2, 1, PLAYERS_LIST.getLastColumn() - 1).getValues()[0];
+        let rowData: any[] = PLAYERS_LIST.getRange(this._rowIndex, 3, 1, PLAYERS_LIST.getLastColumn() - 2).getValues()[0];
         this._name = rowData[0];
         this._items = this.updateItems(rowData);
     }
@@ -55,7 +55,7 @@ class Player {
      * Переводит активное выделение в таблице на строку с именем игрока
      */
     public activate(): void {
-        PLAYERS_LIST.getRange(this._rowIndex, 2).activate();
+        PLAYERS_LIST.getRange(this._rowIndex, 3).activate();
     }
 
     /**
@@ -64,11 +64,11 @@ class Player {
      */
     private updateItems(rowData?: any[]): Item[] {
         if (rowData === undefined) {
-            rowData = PLAYERS_LIST.getRange(this._rowIndex, 2, 1, PLAYERS_LIST.getLastColumn() - 3).getValues()[0];
+            rowData = PLAYERS_LIST.getRange(this._rowIndex, 3, 1, PLAYERS_LIST.getLastColumn() - 2).getValues()[0];
         }
         // сопоставление ПП в заголовке "Обладатели ПП" со списком с листа "Актуальные"
         // затем фильтрация ПП по отмеченным чекбоксам в строке
-        let header: any[] = PLAYERS_LIST.getRange(1, 2, 1, PLAYERS_LIST.getLastColumn() - 3).getValues()[0];
+        let header: any[] = PLAYERS_LIST.getRange(1, 3, 1, PLAYERS_LIST.getLastColumn() - 2).getValues()[0];
         return ItemConstructor.getAllItems(this.sex).filter((item) => {
             if (header.indexOf(item.name) !== -1) {
                 if (rowData[header.indexOf(item.name)]) {
